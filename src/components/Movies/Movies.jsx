6c584e0eef8ea74e8330979d7eacba625afd36e0
Osaze Oviawe <ocubed88@gmail.com>
2022-06-09 00:08:46 +0100

    feat: - rendering list of movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -8,12 +8,38 @@ import {
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '../';
 
 const Movies = () => {
-	const { data } = useGetMoviesQuery();
-	console.log('Movies =======>', data);
+	const { data, error, isFetching } = useGetMoviesQuery();
 
-	return <div>Movies</div>;
+	if (!data && isFetching) {
+		return (
+			<Box display='flex' justifyContent='center'>
+				<CircularProgress size='4rem' />
+			</Box>
+		);
+	}
+
+	if (data?.results?.length === 0) {
+		return (
+			<Box display='flex' justifyContent='center'>
+				<Typography variant='h4'>
+					No movies found!
+					<br />
+					Please search for something else
+				</Typography>
+			</Box>
+		);
+	}
+
+	if (error) return <p>An error has occurred</p>;
+
+	return (
+		<div>
+			<MovieList movies={data} />
+		</div>
+	);
 };
 
 export default Movies;
